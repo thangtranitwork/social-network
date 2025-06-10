00c9cd0d7b584fffc407c7a7ba08da10215fd5de
Thắng Trần <thangtran.it.work@gmail.com>
2025-06-11 00:52:41 +0700

    FIX: uuid length is 36

--- src/main/java/com/stu/socialnetworkapi/validation/validator/UsernameValidator.java
+++ src/main/java/com/stu/socialnetworkapi/validation/validator/UsernameValidator.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class UsernameValidator implements ConstraintValidator<Username, String> {
 
-    private static final String USERNAME_REGEX = "^[a-zA-Z0-9._-]{1,32}$";
+    private static final String USERNAME_REGEX = "^[a-zA-Z0-9._-]{1,36}$";
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {

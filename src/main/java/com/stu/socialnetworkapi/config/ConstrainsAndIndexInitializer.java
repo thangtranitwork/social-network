e39ca274987ec1f2309a7f66cfc0ec06ee4c3b00
Thắng Trần <thangtran.it.work@gmail.com>
2025-06-13 02:07:00 +0700

    FEAT: update constraints

--- src/main/java/com/stu/socialnetworkapi/config/ConstrainsAndIndexInitializer.java
+++ src/main/java/com/stu/socialnetworkapi/config/ConstrainsAndIndexInitializer.java
@@ -17,11 +17,23 @@ public class ConstrainsAndIndexInitializer {
     @PostConstruct
     public void createConstraintsAndIndexes() {
         try (Session session = neo4jDriver.session()) {
+
+            // NODE KEY and RELATIONSHIP KEY are only supported by Neo4j Enterprise Edition
             session.run("CREATE CONSTRAINT user_id_unique IF NOT EXISTS FOR (u:User) REQUIRE u.id IS UNIQUE");
             session.run("CREATE CONSTRAINT user_username_unique IF NOT EXISTS FOR (u:User) REQUIRE u.username IS UNIQUE");
+            session.run("CREATE CONSTRAINT account_id_unique IF NOT EXISTS FOR (a:Account) REQUIRE a.id IS UNIQUE");
             session.run("CREATE CONSTRAINT account_email_unique IF NOT EXISTS FOR (a:Account) REQUIRE a.email IS UNIQUE");
+            session.run("CREATE CONSTRAINT chat_id_unique IF NOT EXISTS FOR (c:Chat) REQUIRE c.id IS UNIQUE");
+            session.run("CREATE CONSTRAINT comment_id_unique IF NOT EXISTS FOR (c:Comment) REQUIRE c.id IS UNIQUE");
             session.run("CREATE CONSTRAINT post_id_unique IF NOT EXISTS FOR (p:Post) REQUIRE p.id IS UNIQUE");
             session.run("CREATE CONSTRAINT file_id_unique IF NOT EXISTS FOR (f:File) REQUIRE f.id IS UNIQUE");
+            session.run("CREATE CONSTRAINT message_id_unique IF NOT EXISTS FOR (m:Message) REQUIRE m.id IS UNIQUE");
+            session.run("CREATE CONSTRAINT notification_id_unique IF NOT EXISTS FOR (n:Notification) REQUIRE n.id IS UNIQUE");
+            session.run("CREATE CONSTRAINT verify_code_id_unique IF NOT EXISTS FOR (v:VerifyCode) REQUIRE v.id IS UNIQUE");
+
+            session.run("CREATE CONSTRAINT friend_uuid_unique IF NOT EXISTS FOR ()-[r:FRIEND]-() REQUIRE r.uuid IS UNIQUE");
+            session.run("CREATE CONSTRAINT block_uuid_unique IF NOT EXISTS FOR ()-[r:BLOCK]-() REQUIRE r.uuid IS UNIQUE");
+            session.run("CREATE CONSTRAINT request_uuid_unique IF NOT EXISTS FOR ()-[r:REQUEST]-() REQUIRE r.uuid IS UNIQUE");
 
             session.run("""
                     CREATE FULLTEXT INDEX userSearchIndex IF NOT EXISTS
@@ -34,10 +46,9 @@ public class ConstrainsAndIndexInitializer {
                     FOR (p:Post)
                     ON EACH [p.content]
                     """);
-            session.run("""
-                        SHOW INDEXES YIELD name, type
-                        RETURN name, type
-                    """);
+
+            log.info("Constraints and indexes created for Neo4j");
+
         } catch (Exception e) {
             log.error("Failed to create constraints and indexes", e);
             throw new RuntimeException("Database initialization failed", e);

765fe9a9def0e2fd802c7859e656c2a3e6711f1d
Thang <thangtran.it.work@gmail.com>
2025-07-11 00:57:51 +0700

    FEAT: rate limit by ip instead of user_id

--- src/main/java/com/stu/socialnetworkapi/config/RateLimitingFilter.java
+++ src/main/java/com/stu/socialnetworkapi/config/RateLimitingFilter.java
@@ -9,8 +9,6 @@ import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,6 +23,7 @@ public class RateLimitingFilter extends OncePerRequestFilter {
 
     private static final int MAX_REQUESTS = 50;        // Giới hạn
     private static final int TIME_WINDOW_SECONDS = 60;  // Mỗi 60 giây
+    private static final String RATE_LIMIT_BY_IP = "rate_limit:ip:";
 
     public RateLimitingFilter(@Qualifier("integerRedisTemplate") RedisTemplate<String, Integer> redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -32,40 +31,52 @@ public class RateLimitingFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        return request.getRequestURI().startsWith("/v1/files") || request.getRequestURI().startsWith("/ws"); // Bỏ qua endpoint /abc
+        return request.getRequestURI().startsWith("/v1/files") || request.getRequestURI().startsWith("/ws");
     }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        if (authentication != null && authentication.isAuthenticated() && !authentication.getName().equals("anonymousUser")) {
-            String username = authentication.getName();
-            String redisKey = "rate_limit:" + username;
+        String clientIp = getClientIP(request);
+        String redisKey = RATE_LIMIT_BY_IP + clientIp;
 
-            Integer currentCount = redisTemplate.opsForValue().get(redisKey);
-            if (currentCount == null) {
-                redisTemplate.opsForValue().set(redisKey, 1, Duration.ofSeconds(TIME_WINDOW_SECONDS));
-                log.debug("Request from {}: {}, name: {}, times {}", request.getRemoteAddr(), request.getRequestURI(), username, redisTemplate.opsForValue().get("rate_limit:" + authentication.getName()));
-            } else if (currentCount < MAX_REQUESTS) {
-                redisTemplate.opsForValue().increment(redisKey);
-                log.debug("Request from {}: {}, name: {}, times {}", request.getRemoteAddr(), request.getRequestURI(), username, redisTemplate.opsForValue().get("rate_limit:" + authentication.getName()));
-            } else {
-                ErrorCode errorCode = ErrorCode.TOO_MANY_REQUESTS;
-                response.setStatus(errorCode.getHttpStatus().value());
-                response.setContentType("application/json");
-                String body = """
-                        {
-                          "code": "%s",
-                          "message": "%s"
-                        }
-                        """.formatted(errorCode.getCode(), errorCode.getMessage());
-                response.getWriter().write(body);
-                return;
-            }
+        Integer currentCount = redisTemplate.opsForValue().get(redisKey);
+        if (currentCount == null) {
+            redisTemplate.opsForValue().set(redisKey, 1, Duration.ofSeconds(TIME_WINDOW_SECONDS));
+            log.debug("New IP {}, request count = 1", clientIp);
+        } else if (currentCount < MAX_REQUESTS) {
+            redisTemplate.opsForValue().increment(redisKey);
+            log.debug("IP {} request count = {}", clientIp, currentCount + 1);
+        } else {
+            // Trả lỗi 429 Too Many Requests
+            ErrorCode errorCode = ErrorCode.TOO_MANY_REQUESTS;
+            response.setStatus(errorCode.getHttpStatus().value());
+            response.setContentType("application/json");
+            String body = """
+                    {
+                      "code": "%s",
+                      "message": "%s"
+                    }
+                    """.formatted(errorCode.getCode(), errorCode.getMessage());
+            response.getWriter().write(body);
+            return;
         }
+
         filterChain.doFilter(request, response);
+    }
+
+    private String getClientIP(HttpServletRequest request) {
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        String remoteAddr = request.getRemoteAddr();
+
+        if (xfHeader != null && !xfHeader.isEmpty()) {
+            log.debug("Client IP from X-Forwarded-For: {}", xfHeader);
+            return xfHeader.split(",")[0];
+        }
+
+        log.debug("Client IP from getRemoteAddr(): {}", remoteAddr);
+        return remoteAddr;
     }
 }
